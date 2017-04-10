fb3977be5b0e61eab661174c2bcb215acfdbf0ce
linnansved <linn_ansved@hotmail.com>
2017-04-10 10:08:52 +0200

    Testproject

---
+++ app/src/main/java/com/gnirt69/firebaseregistrationloginexam/ProfileActivity.java
@@ -0,0 +1,51 @@
+package com.gnirt69.firebaseregistrationloginexam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Button;
+import android.content.Intent;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth firebaseAuth;
+
+    private Button LogOutBtn;
+
+    private TextView tvEmail;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        tvEmail = (TextView) findViewById(R.id.tvEmailProfile);
+        tvEmail.setText(getIntent().getExtras().getString("Email"));
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        if(firebaseAuth.getCurrentUser() == null){
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+
+        //FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        LogOutBtn = (Button) findViewById(R.id.LogOutBtn);
+
+        LogOutBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+
+        if(view == LogOutBtn){
+            firebaseAuth.signOut();;
+            finish();
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
+
+
+}
